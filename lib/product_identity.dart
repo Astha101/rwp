7f6ec352849a65788a4e3f57d5cc03e4f2ba1835
Astha101 <singeraztha123@gmail.com>
2021-09-20 21:07:47 +0545

    application

---
+++ lib/product_identity.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProductIdentity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF3D657),
+          leading: IconButton(
+              icon: Icon(Icons.menu), onPressed: () {}, color: Colors.black),
+          title: Text('Product Identity'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {},
+                color: Colors.black),
+          ],
+        ),
+        backgroundColor: Colors.black,
+        body: Container(
+          child: Text(
+            'Product Identity',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          color: Color(0xFFF3D657),
+          alignment: Alignment.center,
+        ));
+  }
+}
